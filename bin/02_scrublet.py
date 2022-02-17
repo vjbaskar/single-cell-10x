fd9d0e4671aa3461825ab3dc7053cc2bd0d3a5b6
Vijay <vjbaskar@gmail.com>
2022-02-17 14:36:58 +0100

    add scrublet script

---
+++ bin/02_scrublet.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+from pathlib import Path
+
+import numpy as np
+import scanpy as sc
+import scrublet as scr
+
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("--input", required=True)
+    p.add_argument("--output", required=True)
+    p.add_argument("--output-annotated", required=True)
+    p.add_argument("--expected-doublet-rate", type=float, default=0.06)
+    return p.parse_args()
+
+
+def main():
+    args = parse_args()
+    adata = sc.read_h5ad(args.input)
+    counts_matrix = adata.X
+    if not isinstance(counts_matrix, np.ndarray):
+        counts_matrix = counts_matrix.tocsr()
+
+    scrub = scr.Scrublet(counts_matrix, expected_doublet_rate=args.expected_doublet_rate)
+    doublet_scores, predicted_doublets = scrub.scrub_doublets()
+    adata.obs["doublet_score"] = doublet_scores
+    adata.obs["predicted_doublet"] = predicted_doublets.astype(bool)
+
+    Path(args.output_annotated).parent.mkdir(parents=True, exist_ok=True)
+    adata.write_h5ad(args.output_annotated)
+    singlets = adata[~adata.obs["predicted_doublet"]].copy()
+    Path(args.output).parent.mkdir(parents=True, exist_ok=True)
+    singlets.write_h5ad(args.output)
+
+
+if __name__ == "__main__":
+    main()
