b739de2313e73ea4904b996579d56c5300670f34
Vijay <vjbaskar@gmail.com>
2023-11-14 11:22:14 +0100

    use typer umap

--- bin/04_umap.py
+++ bin/04_umap.py
@@ -1,47 +1,44 @@
 #!/usr/bin/env python3
 """Generate UMAP from integrated data."""
 
-import argparse
 from pathlib import Path
 
 import scanpy as sc
+import typer
 
+app = typer.Typer()
 
-def parse_args():
-    p = argparse.ArgumentParser()
-    p.add_argument("--input", required=True)
-    p.add_argument("--output", required=True)
-    p.add_argument("--umap-png", required=True)
-    p.add_argument("--batch-key", default="batch")
-    p.add_argument("--n-neighbors", type=int, default=15)
-    p.add_argument("--leiden-resolution", type=float, default=0.5)
-    return p.parse_args()
-
-
-def main():
+@app.command()
+def main(
+    input: str = typer.Option(...),
+    output: str = typer.Option(...),
+    umap_png: str = typer.Option(...),
+    batch_key: str = typer.Option("batch"),
+    n_neighbors: int = typer.Option(15),
+    leiden_resolution: float = typer.Option(0.5),
+):
     """Compute neighbors, leiden and UMAP."""
-    args = parse_args()
-    adata = sc.read_h5ad(args.input)
+    adata = sc.read_h5ad(input)
 
     if "X_scvi" in adata.obsm:
-        sc.pp.neighbors(adata, use_rep="X_scvi", n_neighbors=args.n_neighbors)
+        sc.pp.neighbors(adata, use_rep="X_scvi", n_neighbors=n_neighbors)
     else:
         sc.pp.pca(adata)
-        sc.pp.neighbors(adata, n_neighbors=args.n_neighbors)
+        sc.pp.neighbors(adata, n_neighbors=n_neighbors)
 
     sc.tl.umap(adata)
-    sc.tl.leiden(adata, resolution=args.leiden_resolution)
+    sc.tl.leiden(adata, resolution=leiden_resolution)
 
     colors = ["leiden"]
-    if args.batch_key in adata.obs.columns:
-        colors.append(args.batch_key)
+    if batch_key in adata.obs.columns:
+        colors.append(batch_key)
     fig = sc.pl.umap(adata, color=colors, show=False, return_fig=True)
-    Path(args.umap_png).parent.mkdir(parents=True, exist_ok=True)
-    fig.savefig(args.umap_png, dpi=200, bbox_inches="tight")
+    Path(umap_png).parent.mkdir(parents=True, exist_ok=True)
+    fig.savefig(umap_png, dpi=200, bbox_inches="tight")
 
-    Path(args.output).parent.mkdir(parents=True, exist_ok=True)
-    adata.write_h5ad(args.output)
+    Path(output).parent.mkdir(parents=True, exist_ok=True)
+    adata.write_h5ad(output)
 
 
 if __name__ == "__main__":
-    main()
+    app()
